e2a3c6df23b74909e6f68cd4d58f7e0bf9ce80e6
MatiasMaron <46878355@est.ort.edu.ar>
2024-03-12 20:16:17 -0300

    Conexion a la base de datos finalizada

--- registro/conexion.php
+++ registro/conexion.php
@@ -1,3 +1,3 @@
 <?php
-    $conex = mysqli_connect("localhost", "root", "", "registro");
+    $conex = mysqli_connect("b9lgoxjujerpeef9g7zt-mysql.services.clever-cloud.com", "uricnqyt3xjaztlp", "ARWG8EopSExi3WNtv6X0", "b9lgoxjujerpeef9g7zt");
 ?>
